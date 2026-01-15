34588ea3fe46ed0f9ec5757c8e7ab6a5eddfa7d1
kharitonovmaximr-afk <kharitonovmaximr@gmail.com>
2026-01-15 21:28:39 +0300

    Add files via upload

---
+++ Registration/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for Registration project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from users.views import register_user, profile_user, login_user
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', register_user, name='signup'),
+    path('profile/', profile_user, name='profile'),
+    path('login/', login_user, name='login'),
+    path('training/', include('training.urls')),
+    path('gto/', include('gto.urls')),
+    path('coop/', include('coop.urls')),
+    path('main/', include('main.urls')),
+    path('program/', include('program.urls')),
+]
+
+
